1f1388d005c848580d19d0fa14df3ba1d6057857
Maksolo27 <71383659+Maksolo27@users.noreply.github.com>
2021-05-23 15:16:18 +0300

    Added post-method

--- src/main/java/com/country/webcountryproj/controller/CountryController.java
+++ src/main/java/com/country/webcountryproj/controller/CountryController.java
@@ -4,24 +4,40 @@ import com.country.webcountryproj.Country;
 import com.country.webcountryproj.service.CountryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/countries")
+@RequestMapping()
 @RequiredArgsConstructor
 public class CountryController {
 
     @Autowired
-    private CountryServiceImpl counryService;
+    private CountryServiceImpl countryService;
+    private Country currCountry;
 
 
-    @GetMapping
-    public String getCountryByCode(Model model){
-        Country country = counryService.getCountryByCode("CRI");
+    @PostMapping("/inPindex")
+    public String postCountry(Country country){
+        currCountry = countryService.getCountryByCode(country.getCode());
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String getCountry(Model model){
+        System.out.println("get");
+        Country country = countryService.getCountryByCode(currCountry.getCode());
         model.addAttribute("country", country);
         return "index";
     }
+
 }
